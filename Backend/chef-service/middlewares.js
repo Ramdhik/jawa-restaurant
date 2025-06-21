dcb105355fd7e12f0b38187257789f264c329e71
Viannn <luvdasyapra@gmail.com>
2025-06-21 11:01:18 +0700

    Modifikasi pengambilan token dan userId

--- Backend/chef-service/middlewares.js
+++ Backend/chef-service/middlewares.js
@@ -3,18 +3,6 @@ const jwt = require('jsonwebtoken');
 module.exports = function (req, res, next) {
   const token = req.header('Authorization')?.split(' ')[1]; // "Bearer <token>"
 
-  // // Mengambil token dari header
-  // const authHeader = req.header('Authorization');
-  // if (authHeader && authHeader.startsWith('Bearer ')) {
-  //     token = authHeader.split(' ')[1];
-  // }
-
-  // // Jika tidak ada di header, coba dari HttpOnly cookie
-  // // Pastikan Anda sudah menggunakan 'cookie-parser' di app.js
-  // if (!token && req.cookies && req.cookies.jwt) { // 'jwt' adalah nama cookie Anda
-  //     token = req.cookies.jwt;
-  // }
-
   if (!token) {
     return res.status(401).json({ message: 'Akses ditolak. Token tidak ada.' });
   } 
